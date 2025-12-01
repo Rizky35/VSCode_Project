2c5d870021805328749d86e8d06b4c97030f5324
Rizky35 <rizkyananda88767@gmail.com>
2025-12-01 04:43:31 +0000

    ignore update

--- STSI4303 - Pemrograman Berbasis Perangkat Bergerak/tugas2/resful-api/dist/controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userController = void 0;
-const User_1 = require("./User/User");
-const userController = new User_1.UserController();
-exports.userController = userController;
